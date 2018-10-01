899b578549bf6cd7251bbe620a18ef18edc5a193
gorniczy <denysgornichar@gmail.com>
2018-10-02 00:12:03 +0200

    add complete "Add New Properties to a JavaScript Object" challenge

--- JavaScript.js
+++ JavaScript.js
@@ -527,3 +527,59 @@ console.log(result);
 console.log(digit);
 
 //  Add New Properties to a JavaScript Object
+
+var collection = {
+    "2548": {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [
+        "Let It Rock",
+        "You Give Love a Bad Name"
+      ]
+    },
+    "2468": {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [
+        "1999",
+        "Little Red Corvette"
+      ]
+    },
+    "1245": {
+      "artist": "Robert Palmer",
+      "tracks": [ ]
+    },
+    "5439": {
+      "album": "ABBA Gold"
+    }
+};
+
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+
+function updateRecords(id, prop, value) {
+  if(prop == "album" && value != "") {
+    collection[id]["album"] = value;
+  }
+
+  else if(prop == "artist" && value != "") {
+    collection[id]["artist"] = value;
+  }
+
+  else if(prop == "tracks" && collection[id].hasOwnProperty(prop) == false) {
+    collection[id][prop] = [];
+    collection[id][prop].push(value);
+  }
+
+  else if(prop == "tracks" && collection[id]["tracks"] == "") {
+    collection[id][prop].push(value);
+  }
+
+  else if(value == "") {
+    delete collection[id][prop];
+  }
+
+  return collection;
+}
+
+updateRecords(5439, "artist", "ABBA");
