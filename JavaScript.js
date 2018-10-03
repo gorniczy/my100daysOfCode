9b1ff39c1da69ff3d3a3a6a44d21af8e9b0fc8e4
gorniczy <denysgornichar@gmail.com>
2018-10-03 23:34:38 +0200

    add "Write Higher Order Arrow Functions"

--- JavaScript.js
+++ JavaScript.js
@@ -593,3 +593,17 @@ do {
   myArray.push(i);
   i++;
 } while (i < 10);
+
+// Write Higher Order Arrow Functions
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+
+  const integers = arr.filter((num) => Number.isInteger(num) && num > 0);
+
+  return squaredIntegers;
+};
+
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
